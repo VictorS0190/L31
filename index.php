be0a72f2f737aad0ccc62c826b77bf8d79ce5252
VictorS0190 <InfernapeFL@hotmail.com>
2025-05-11 10:01:16 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Hola Mundo</title>
+</head>
+<body>
+    <h1>
+        <?php echo "¡Hola Mundo!"; ?>
+    </h1>
+</body>
+</html>
